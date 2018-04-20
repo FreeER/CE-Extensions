03196338220309dd8bc647dd6e9464a295f9ffc1
Benjamin Clements <HJP_Ben1993@yahoo.com>
2018-04-20 07:55:36 -0500

    disable mono menu option

---
+++ disableMono.lua
@@ -0,0 +1,36 @@
+--##### Disable Mono Lua Script for Cheat Engine
+--##### Author: FreeER
+--##### Github: https://github.com/FreeER
+--##### Website: https://www.facebook.com/groups/CheatTheGame
+--##### YouTube: https://www.youtube.com/channel/UCLy60mbqc3rSvh42jkpCDxw
+--[[
+  adds a menu option to remove all hotkeys from memory records
+  note: this does not remove/destroy any hotkeys created by lua code only the gui
+]]
+
+-- START OF USER CONFIG --
+local useGroupMenu = true
+-- END OF CONFIG --
+
+-- START OF EXT TEMPLATE CONFIG --
+local extItemCaption = 'Disable'
+-- START OF EXT TEMPLATE CONFIG --
+
+-- menu code --
+local extMenuItem = createMenuItem(extMenu)
+extMenuItem.Caption = extItemCaption
+
+local t = createTimer()
+t.Interval = 1000
+t.OnTimer = function(t)
+  if not miMonoTopMenuItem then return end
+  extMenu = miMonoTopMenuItem
+  extMenu.add(extMenuItem)
+  t.destroy()
+end
+-- menu code --
+
+extMenuItem.OnClick = function()
+  monopipe.destroy()
+  monopipe = nil
+end
