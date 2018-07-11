907d496113e07b16ba503e0fa9d273abf6c699da
Benjamin Clements <2312577+FreeER@users.noreply.github.com>
2018-07-11 12:21:09 -0500

    update to enable users to disable saving when designing forms due to selection reset on save...

--- autosave.lua
+++ autosave.lua
@@ -17,6 +17,11 @@
 local numSaves = 5
 local tableSaveTime = 5 -- every n seconds
 local luaEngineDelay = 5 -- seconds since typing to save
+local saveInFormDesigner = true -- seems to reset selected items and properties/events scrollbar
+-- ^ alternative is to open lua engine and type FreeER.autosave.tableSaves.timer.enabled = false
+-- that does however completely disable the table saves, not just when the FormDesigner is open
+-- so do not forget to reenable them :)
+-- you can also access the lua script timer with FreeER.autosave.luaSaves.timer
 ----------------------------------------------------------------------
 ---------------------------  END OF CONFIGURATION --------------------
 ----------------------------------------------------------------------
@@ -97,10 +102,53 @@ local function Update_Lua_Bak_Timer(t)
   end
 end
 
+local function getFormDesigner()
+  for i=0,getFormCount()-1 do
+    local f = getForm(i)
+    if(f.name == 'FormDesigner') then return f end
+  end
+end
+
+--[[
+local function addMultiSelectedItems(t, items)
+  for i=0,items.Count-1 do
+    local it = items[i]
+    if it.MultiSelected then t[#t+1] = it end
+    if it.HasChildren and it.items then addMultiSelectedItems(t, it.items) end
+  end
+end
+]]
+  --[[ saveTable resets FormDesigner selection... save and restore it
+  local Selected = nil
+  local MultiSelected = {}
+  if FormDesigner then
+    local insp = FormDesigner.ObjectInspectorDlg
+    local tree = insp.ComponentTree
+    Selected = tree.getSelected()
+    addMultiSelectedItems(MultiSelected, tree.Items)
+  end]]
+
+  --[[ restore selection
+  if FormDesigner then
+    local insp = FormDesigner.ObjectInspectorDlg
+    local tree = insp.ComponentTree
+    local first = tree.Items[0]
+    first.Selected = false
+    first.MultiSelected = false
+    tree.Selected = Selected
+    for _,item in ipairs(MultiSelected) do item.MultiSelected = true end
+  end
+  ]]
+
 local function Write_Table_Bak()
   local Bak = tableSaves.peek()
   --print('saving table to', Bak)
+
+  local FormDesigner = getFormDesigner()
+  if FormDesigner and not saveInFormDesigner then return end
+
   saveTable(Bak)
+
   local prv = tableSaves.prev()
   -- if they are the same then next time just overwrite the same one
   local file = io.open(prv)
@@ -129,3 +177,6 @@ mScript.OnChange = function()
 end
 
 tableSaves.timer.OnTimer = Write_Table_Bak
+FreeER.autosave = {}
+FreeER.autosave.luaSaves = luaSaves
+FreeER.autosave.tableSaves = tableSaves
